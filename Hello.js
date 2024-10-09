f5c5e3394cd7376b0063236050d68b495fc4e7dd
GopiKrishna <gopi.krishna@kalgudi.com>
2024-10-09 11:42:47 +0530

    add testing file

--- Hello.js
+++ Hello.js
@@ -1,10 +1,8 @@
 const a = "apple";
-const b="ball";
-var s="satti";
-var s="hitman";
-var s="Ironman";
-var s="hitman rohit";
-let r="ravi1";
-let r="ravi2";
-
-
+const b = "ball";
+var s = "satti";
+var s = "hitman";
+var s = "Ironman";
+var s = "hitman rohit";
+let r = "ravi1";
+let r1 = "ravi2";
